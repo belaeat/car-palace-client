0fedf5c674cf0037897ae1f01ccfe2fde3844b20
belaeat <omyhossain@gmail.com>
2023-05-21 00:47:38 +0600

    demo json added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,28 @@
+import { useContext } from 'react';
 import loginImage from '../../assets/login.jpg'
 import { Link } from "react-router-dom";
+import { AuthContext } from '../../providers/AuthProvider';
 
 const SignUp = () => {
+
+    const { createUser } = useContext(AuthContext)
+
+    const handleSignUp = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password)
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user
+                console.log(user)
+            })
+            .catch(error => console.log(error))
+    }
+
     return (
         <div className='relative'>
             <img src={loginImage} alt="" />
@@ -12,7 +33,7 @@ const SignUp = () => {
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={handleSignUp}>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Name</span>
@@ -35,7 +56,7 @@ const SignUp = () => {
                                     <label className="label">
                                         <span className="label-text">Photo URL</span>
                                     </label>
-                                    <input type="password" name='photo' placeholder="Your photo URL" className="input input-bordered" required />
+                                    <input type="text" name='photo' placeholder="Your photo URL" className="input input-bordered" required />
                                 </div>
                                 <div className="form-control mt-6">
                                     <button className="btn bg-[#86C8BC] border-none">Sign Up</button>
