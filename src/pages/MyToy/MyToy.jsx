a232c1ba3a1ba2cb5ad438d21f93fec52bd74edf
belaeat <omyhossain@gmail.com>
2023-05-23 18:38:23 +0600

    my toys working fine

--- src/pages/MyToy/MyToy.jsx
+++ src/pages/MyToy/MyToy.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import MyToysRow from "./MyToysRow";
 
 
 const MyToy = () => {
@@ -7,17 +8,47 @@ const MyToy = () => {
     const { user } = useContext(AuthContext)
     const [myToys, setMyToys] = useState([])
 
-    const url = `http://localhost:5000/newAddedToy?email=${user?.email}`
+    const url = `http://localhost:5000/newAddedToy?email=${user?.sellerEmail}`
 
     useEffect(() => {
         fetch(url)
-        .then (res => res.json())
-        .then( data => setMyToys(data))
+            .then(res => res.json())
+            .then(data => setMyToys(data))
     }, [])
 
     return (
         <div>
             <h2>My Toys : {myToys.length}</h2>
+
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Toy Name</th>
+                            <th>Price</th>
+                            <th>Rating</th>
+                            <th>Quantity</th>
+                            <th>Sub Category</th>
+                            <th>Update</th>
+                        </tr>
+                    </thead>
+                    <tbody>                        
+                           {
+                            myToys.map(toys => <MyToysRow
+                                key={toys._id}
+                                toys={toys}
+                            ></MyToysRow>)
+                           }                     
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/pages/MyToy/MyToysRow.jsx
@@ -0,0 +1,33 @@
+
+
+const MyToysRow = ({ toys }) => {
+
+    const { toyName, toyPicture, subcategory, price, rating, availableQuantity } = toys
+
+    return (
+        <tr>
+            <th>
+                <label>
+                    <input type="checkbox" className="checkbox" />
+                </label>
+            </th>
+            <td>
+                <div className="avatar">
+                    <div className="rounded w-full h-24">
+                        <img src={toyPicture} alt="Avatar Tailwind CSS Component" />
+                    </div>
+                </div>
+            </td>
+            <td>{toyName}</td>
+            <td>{price}</td>
+            <td>{rating}</td>
+            <td>{availableQuantity}</td>
+            <td>{subcategory}</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+        </tr>
+    );
+};
+
+export default MyToysRow;
